d8f59f7d43f92cf05ff8265c75e242e4b139bb84
István Szabó <istvan751108@gmail.com>
2022-05-17 19:53:06 +0200

    Starter commit

---
+++ src/main/java/hu/nye/progkor/dvdcatalouge/DvdCatalougeApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.dvdcatalouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdCatalougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdCatalougeApplication.class, args);
+	}
+
+}
